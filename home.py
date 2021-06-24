e04d23658e381e384b58c42f5bd5b1f5bf1497bd
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2021-06-24 12:10:37 -0300

    Arquivo para o professor Selles

--- home.py
+++ home.py
@@ -1,1 +1,14 @@
-print('esse aqui,aula de brandao correeeeeee')
+import numpy as plt
+import pandas as pd   
+import matplotlib.pyplot as plt 
+
+dados = pd.read_csv('/home/daniel/Downloads/athlete_events.csv')
+
+masculinos = dados.loc[dados['Sex']=='M']
+
+altura = masculinos['Height']
+peso = masculinos['Weight']
+
+plt.scatter(altura, peso)
+plt.savefig('grafico_linha.png', format='png')
+plt.show()
