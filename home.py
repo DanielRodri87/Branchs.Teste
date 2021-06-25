dd232e9029990b31fa20595c9c7db7d13d5a55f0
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-06-25 09:38:16 -0300

    Atualização programa prof

--- home.py
+++ home.py
@@ -2,13 +2,16 @@ import numpy as plt
 import pandas as pd   
 import matplotlib.pyplot as plt 
 
-dados = pd.read_csv('/home/daniel/Downloads/athlete_events.csv')
+dados = pd.read_csv('C:\\Users\\Rodrigues\\Desktop\\BRANCH-TESTE\\athlete_events.csv')
 
 masculinos = dados.loc[dados['Sex']=='M']
 
 altura = masculinos['Height']
 peso = masculinos['Weight']
 
+plt.title('Reção Altura/Peso')
+plt.ylabel('Altura')
+plt.xlabel('Peso')
 plt.scatter(altura, peso)
 plt.savefig('grafico_linha.png', format='png')
 plt.show()
