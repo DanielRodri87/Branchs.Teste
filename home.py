35fae52ccc3050435659a5948f7adf04775f7967
Daniel <daniel.acaua.piaui.brasil@gmail.com>
2021-06-25 21:26:27 -0300

    Arquivo final

--- home.py
+++ home.py
@@ -1,22 +1,27 @@
-import numpy as plt
+#Importação das bibliotcas necessáiras 
 import pandas as pd   
 import matplotlib.pyplot as plt 
 
+#Arquivo com dados de atletas olímpicos usando no programa
 dados = pd.read_csv('C:\\Users\\Rodrigues\\Desktop\\BRANCH-TESTE\\athlete_events.csv')
 
+#seleção de gênero
 masculinos = dados.loc[dados['Sex']=='M']
 femininos = dados.loc[dados['Sex']=='F']
 
+#Seleção de tópicos masculinos abordados
 alturaM = masculinos['Height']
-pesoM = masculinos['Weight']
+idadeM = masculinos['Age']
 
+#Seleção de tópicos femininos abordados
 alturaF = femininos['Height']
-pesoF = femininos['Weight']
+idadeF = femininos['Age']
 
-plt.title('Reção Altura/Peso')
-plt.ylabel('Altura')
-plt.xlabel('Peso')
-plt.scatter(alturaF, pesoF, color="pink")
-plt.scatter(alturaM, pesoM, color="blue")
-plt.savefig('grafico_linha.png', format='png')
+#Criação e amarzenamengo do gráfico
+plt.title('Tamanho Feminino e Masculino em relação a Idade')
+plt.ylabel('Idade')
+plt.xlabel('Tamanho')
+plt.scatter(alturaF, idadeF, color="pink") #Mulheres simbolizadas por rosa
+plt.scatter(alturaM, idadeM, color="blue") #homens simbolizados por azul
+plt.savefig('atletas.png', format='png')
 plt.show()
